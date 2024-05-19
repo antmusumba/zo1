bba0db7d2a88ce8d76e470f1852baeb70d3b9d42
antmusumba <musumbaantony3@gmail.com>
2024-05-19 14:42:20 +0300

    add DescendComb function

---
+++ utils/descendComb.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func DescendComb2() {
+	for i := '9'; i >= '0'; i-- {
+		for j := '9'; j >= '0'; j-- {
+			for k := '9'; k >= '0'; k-- {
+				for l := '9'; l >= '0'; l-- {
+					if (i == k && j <= l) || i < k {
+						continue
+					}
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(l)
+					if !(i == '0' && j == '0' && k == '0' && l == '1') {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
---
+++ utils/max.go
@@ -0,0 +1,12 @@
+package utils
+
+func Max(a []int) int {
+	for i := 0; i < len(a); i++ {
+		for j := i + 1; j < len(a); j++ {
+			if a[i] > a[j] {
+				a[i], a[j] = a[j], a[i]
+			}
+		}
+	}
+	return a[len(a)-1]
+}
