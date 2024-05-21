e37ebc1bd6ca0bd14291bb74eda26793fc80bebe
antmusumba <musumbaantony3@gmail.com>
2024-05-21 11:42:47 +0300

    add a function that checks if a number passed to it is a prime number

---
+++ utils/isprime.go
@@ -0,0 +1,19 @@
+package utils
+
+func Isprime(num int) bool {
+	if num < 2 {
+		return false
+	}
+	if num == 2 || num == 3 {
+		return true
+	}
+	if num%2 == 0 {
+		return false
+	}
+	for i := 3; i*i <= num; i += 2 {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
